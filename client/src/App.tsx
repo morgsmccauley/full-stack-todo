5eb885978269f5568f66428fadd0e3115586bdfe
Morgan McCauley <morgan@mccauley.co.nz>
2020-07-31 13:58:57 +1200

    Query and render ToDos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
 import './App.css';
 
+const TO_DOS = gql`
+  query {
+    toDos {
+      id
+      label
+      done
+    }
+  }
+`;
+
+type ToDo = {
+  id: String,
+  label: String,
+  done: Boolean
+}
+
 function App() {
+  const { data } = useQuery(TO_DOS);
   return (
     <div className="App">
       <header className="App-header">
-        <p>hi</p>
+        {data?.toDos.map((toDo: ToDo) => (
+          <>
+            <p>{toDo.id}</p>
+            <p>{toDo.label}</p>
+            <p>{toDo.done.toString()}</p>
+          </>
+        ))}
       </header>
     </div>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import App from './App';
+
+import './index.css';
+
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:8080/graphql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
